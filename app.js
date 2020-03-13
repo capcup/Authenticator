caec62743e17e2e779397fa3df20a53bcd892685
capcup <cemcap10@gmail.com>
2020-03-13 11:47:40 +0100

    Implement registration

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -19,6 +21,25 @@ app.set('view engine', 'ejs');
 // Bodyparser 
 app.use(express.urlencoded( { extended: false} ));
 
+// Express Session 
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Connect flash 
+app.use(flash());
+
+// Global Vars 
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash("error_msg");
+    next();
+});
+
 // Routes
 app.use('/', require('./routes/index'))
 app.use("/users", require("./routes/users"));
@@ -26,3 +47,5 @@ app.use("/users", require("./routes/users"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server startet on port ${PORT}`));
+
+// https://www.youtube.com/watch?v=6FOq4cUdH8k&41m0s
